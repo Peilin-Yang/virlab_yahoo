45f09b5b2263b5090315a9efff3aece1772ca591
Peilin Yang <yangpeilyn@gmail.com>
2016-05-13 09:51:06 -0400

    add query processing

--- process_data.py
+++ process_data.py
@@ -6,6 +6,7 @@ import inspect
 from inspect import currentframe, getframeinfo
 import subprocess
 import shlex
+from operator import itemgetter
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -40,18 +41,39 @@ class Process(object):
             self.output_doc(os.path.join(self.corpus_path, 'corpus', 'raw'), docno, doc['rawText'])
             word_vec = ' '.join(doc['rawText'].split()[:30])
             self.output_doc(os.path.join(self.corpus_path, 'corpus', 'first30'), docno, word_vec)
-            
+
+    def output_judgement(self):
+        """
+        For each query, output all the documents that occur at least 
+        once in its judgement. Try to count the times the document gets 
+        positive and negative; We might want a ranking list though.
+        """
+        with open( os.path.join(self.corpus_path, 'json', 'unique_query.json') ) as f:
+            queries = json.load(f)
+            unique_query = {}
+            for query in queries:
+                unique_query[query] = []
+                for docid in queries[query]:
+                    unique_query[query].append( (docid, queries[query][docid]['pos_cnt']) )
+            for query in unique_query:
+                unique_query[query].sort(key=itemgetter(1, 0), reverse=True)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-o", "--output_documents_for_index",
         nargs=1,
-        help="Re-generate MicroBlog Corpus")
+        help="")
+
+    parser.add_argument("-j", "--output_judgement",
+        nargs=1,
+        help="See the explanations in function definition")
 
     args = parser.parse_args()
 
     if args.output_documents_for_index:
         Process(args.output_documents_for_index[0]).output_documents_for_index()
-
+    if args.output_judgement:
+        Process(args.output_judgement[0]).output_judgement()
 
